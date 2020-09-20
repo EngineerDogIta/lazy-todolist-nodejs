42fd0567ce925ba41e079d8231806cc3cb9f8f00
Yari D'Onofrio <bellayari94@gmail.com>
2020-09-20 17:25:12 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,20 @@
 var pg = require('pg');
-pg.defaults.ssl = true;
+const sequelize = ('sequelize');
 
-const {
-    Sequelize
-} = require('sequelize');
+pg.defaults.ssl = true;
 
 var sequelize;
 
 if (process.env.DATABASE_URL) {
     console.log('connecting to postgres');
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: true
-        }
-    });    
+    // sequelize = new Sequelize(process.env.DATABASE_URL, {
+    //     dialect: 'postgres',
+    //     protocol: 'postgres',
+    //     dialectOptions: {
+    //         ssl: true
+    //     }
+    // });    
+    sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
     console.log('sqlite local db');
     sequelize = new Sequelize({
