34aca9ecc5ccec33a48147b7e9db9b678e699e07
Yari D'Onofrio <bellayari94@gmail.com>
2020-09-26 22:06:50 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,26 +1,21 @@
-var pg = require('pg');
+
 const { Sequelize } = require('sequelize');
-
-pg.defaults.ssl = true;
-
+var env = process.env.NODE_ENVl;
 var sequelize;
 
-if (process.env.DATABASE_URL) {
-    console.log('connecting to postgres');
-    //sequelize = new Sequelize(process.env.DATABASE_URL);
+if (env === 'production') {
+    console.log('hosting on heroku using postgres');
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: true
-        }
-    });
+        dialect: 'postgres'
+    })
 } else {
-    console.log('sqlite local db');
+    console.log('localhost using sqlite');
     sequelize = new Sequelize({
         dialect: 'sqlite',
         storage: './data/database.sqlite'
     });
 }
+
+
 
 module.exports = sequelize;
