422d4e8218906c0fdb469838ccc1e28f5a280dfc
Toyota Yaris 2009 <yaris2009@toyota.it>
2025-03-22 12:30:15 +0100

    refactor: cleanup project structure and reorganize directories

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'components/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', taskRoutes);
